eae06ba7f83fa501b41089ab600c7311f33db6ff
radhikaepiq <103490335+radhikaepiq@users.noreply.github.com>
2023-11-09 19:45:25 +0530

    Update tc_getapi.tf

--- tc_getapi.tf
+++ tc_getapi.tf
@@ -12,6 +12,9 @@ provider "aws" {
 resource "aws_api_gateway_rest_api" "RadDemoAPI" {
   name        = "RadDemoAPI"
   description = "This is my API for demonstration purposes"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_resource" "RadDemoResource" {
